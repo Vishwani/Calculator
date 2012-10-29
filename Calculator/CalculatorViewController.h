5eeac11529a52fcbfb48a2b1a40041f805d075b6
Samar Singla <samarsingla@Samars-Mac-mini.local>
2012-10-29 17:22:06 +0530

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Samar Singla on 10/29/12.
+//  Copyright (c) 2012 Samar Singla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Samar Singla on 10/29/12.
+//  Copyright (c) 2012 Samar Singla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CalculatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CalculatorAppDelegate class]));
+    }
+}
